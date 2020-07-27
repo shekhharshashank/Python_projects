059a8d0a1d1399cc7d16c54a763adb5bca642449
Shashank Shekhar <ShashankShekhar@gmail.com>
2020-07-27 13:09:04 +0530

    Adding Code1

--- shashank.py
+++ shashank.py
@@ -1,2 +1,15 @@
 import datetime
 print (datetime.datetime.now())
+print ("Date Time Is:",datetime.datetime.now())
+print ("Printed The Dates")
+
+#Example2
+x=10
+y="10"
+z=10.2
+
+sum1=x+x
+sum2=y+y
+print(sum1,sum2)
+
+print(type(x),type(y),type(z))
